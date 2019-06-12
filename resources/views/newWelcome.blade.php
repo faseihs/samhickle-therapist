6b263a8c7a2c81812fe6c1db25f01f45af78242d
Faseih Saad <fsaad.bese15seecs@seecs.edu.pk>
2019-06-12 22:01:21 +0500

    Removed COntent in Home Page

--- resources/views/newWelcome.blade.php
+++ resources/views/newWelcome.blade.php
@@ -126,204 +126,13 @@
     </div>
     <!-- /container -->
 
-    <div class="bg_color_1">
-        <div class="container margin_120_95">
-            <div class="main_title">
-                <h2>Most Viewed doctors</h2>
-                <p>Usu habeo equidem sanctus no. Suas summo id sed, erat erant oporteat cu pri.</p>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="box_list home">
-                        <a href="#0" data-toggle="tooltip" data-placement="top" title="Add to wishlist" class="wish_bt"></a>
-                        <figure>
 
-                            <a href="detail-page.html"><img src="http://via.placeholder.com/565x565.jpg" class="img-fluid" alt=""></a>
-                            <div class="preview"><span>Read more</span></div>
-                        </figure>
-                        <div class="wrapper">
-                            <small>Psicologist</small>
-                            <h3>Dr. Watamaniuk</h3>
-                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
-                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
-                        </div>
-                        <ul>
-                            <li><i class="icon-eye-7"></i> 854 Views</li>
-                            <li><a href="detail-page.html">Book now</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="box_list home">
-                        <a href="#0" data-toggle="tooltip" data-placement="top" title="Add to wishlist" class="wish_bt"></a>
-                        <figure>
-
-                            <a href="detail-page.html"><img src="http://via.placeholder.com/565x565.jpg" class="img-fluid" alt=""></a>
-                            <div class="preview"><span>Read more</span></div>
-                        </figure>
-                        <div class="wrapper">
-                            <small>Psicologist</small>
-                            <h3>Dr. Mantooth</h3>
-                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
-                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
-                        </div>
-                        <ul>
-                            <li><i class="icon-eye-7"></i> 854 Views</li>
-                            <li><a href="detail-page.html">Book now</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="box_list home">
-                        <a href="#0" data-toggle="tooltip" data-placement="top" title="Add to wishlist" class="wish_bt"></a>
-                        <figure>
-
-                            <a href="detail-page.html"><img src="http://via.placeholder.com/565x565.jpg" class="img-fluid" alt=""></a>
-                            <div class="preview"><span>Read more</span></div>
-                        </figure>
-                        <div class="wrapper">
-                            <small>Psicologist</small>
-                            <h3>Dr. Pullman</h3>
-                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
-                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_2.svg" width="15" height="15" alt=""></a>
-                        </div>
-                        <ul>
-                            <li><i class="icon-eye-7"></i> 854 Views</li>
-                            <li><a href="detail-page.html">Book now</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="box_list home">
-                        <a href="#0" data-toggle="tooltip" data-placement="top" title="Add to wishlist" class="wish_bt"></a>
-                        <figure>
-
-                            <a href="detail-page.html"><img src="http://via.placeholder.com/565x565.jpg" class="img-fluid" alt=""></a>
-                            <div class="preview"><span>Read more</span></div>
-                        </figure>
-                        <div class="wrapper">
-                            <small>Psicologist</small>
-                            <h3>Dr. Toothaker</h3>
-                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
-                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_6.svg" width="15" height="15" alt=""></a>
-                        </div>
-                        <ul>
-                            <li><i class="icon-eye-7"></i> 854 Views</li>
-                            <li><a href="detail-page.html">Book now</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="box_list home">
-                        <a href="#0" data-toggle="tooltip" data-placement="top" title="Add to wishlist" class="wish_bt"></a>
-                        <figure>
-
-                            <a href="detail-page.html"><img src="http://via.placeholder.com/565x565.jpg" class="img-fluid" alt=""></a>
-                            <div class="preview"><span>Read more</span></div>
-                        </figure>
-                        <div class="wrapper">
-                            <small>Psicologist</small>
-                            <h3>Dr. Brilliant</h3>
-                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
-                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_6.svg" width="15" height="15" alt=""></a>
-                        </div>
-                        <ul>
-                            <li><i class="icon-eye-7"></i> 854 Views</li>
-                            <li><a href="detail-page.html">Book now</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="box_list home">
-                        <a href="#0" data-toggle="tooltip" data-placement="top" title="Add to wishlist" class="wish_bt"></a>
-                        <figure>
-
-                            <a href="detail-page.html"><img src="http://via.placeholder.com/565x565.jpg" class="img-fluid" alt=""></a>
-                            <div class="preview"><span>Read more</span></div>
-                        </figure>
-                        <div class="wrapper">
-                            <small>Psicologist</small>
-                            <h3>Dr. Crownover</h3>
-                            <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cuodo....</p>
-                            <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
-                            <a href="#0" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
-                        </div>
-                        <ul>
-                            <li><i class="icon-eye-7"></i> 854 Views</li>
-                            <li><a href="detail-page.html">Book now</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <!-- /row -->
-            <p class="text-center add_top_30"><a href="list.html" class="btn_1 medium">View all Doctors</a></p>
-        </div>
-        <!-- /container -->
-    </div>
     <!-- /white_bg -->
 
-    <div class="container margin_120_95">
-        <div class="main_title">
-            <h2>Find your doctor or clinic</h2>
-            <p>Nec graeci sadipscing disputationi ne, mea ea nonumes percipitur. Nonumy ponderum oporteat cu mel, pro movet cetero at.</p>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-xl-4 col-lg-5 col-md-6">
-                <div class="list_home">
-                    <div class="list_title">
-                        <i class="icon_pin_alt"></i>
-                        <h3>Search by City or Zone</h3>
-                    </div>
-                    <ul>
-                        <li><a href="#0"><strong>23</strong>Albany</a></li>
-                        <li><a href="#0"><strong>23</strong>Albuquerque</a></li>
-                        <li><a href="#0"><strong>23</strong>Atlanta</a></li>
-                        <li><a href="#0"><strong>23</strong>Baltimore</a></li>
-                        <li><a href="#0"><strong>23</strong>Baton Rouge</a></li>
-                        <li><a href="#0"><strong>23</strong>Birmingham</a></li>
-                        <li><a href="#0"><strong>23</strong>Boston</a></li>
-                        <li><a href="#0"><strong>23</strong>Buffalo</a></li>
-                        <li><a href="#0"><strong>23</strong>Charleston</a></li>
-                        <li><a href="#0">More...</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-5 col-md-6">
-                <div class="list_home">
-                    <div class="list_title">
-                        <i class="icon_archive_alt"></i>
-                        <h3>Search by type</h3>
-                    </div>
-                    <ul>
-                        <li><a href="#0"><strong>23</strong>Allergist</a></li>
-                        <li><a href="#0"><strong>23</strong>Cardiologist</a></li>
-                        <li><a href="#0"><strong>23</strong>Chiropractor</a></li>
-                        <li><a href="#0"><strong>23</strong>Dentist</a></li>
-                        <li><a href="#0"><strong>23</strong>Dermatologist</a></li>
-                        <li><a href="#0"><strong>23</strong>Gastroenterologist</a></li>
-                        <li><a href="#0"><strong>23</strong>Ophthalmologist</a></li>
-                        <li><a href="#0"><strong>23</strong>Optometrist</a></li>
-                        <li><a href="#0"><strong>23</strong>Pediatrician</a></li>
-                        <li><a href="#0">More....</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!-- /row -->
-    </div>
+
     <!-- /container -->
 
-    <div id="app_section">
+    {{--<div id="app_section">
         <div class="container">
             <div class="row justify-content-around">
                 <div class="col-md-5">
@@ -347,7 +156,7 @@
             <!-- /row -->
         </div>
         <!-- /container -->
-    </div>
+    </div>--}}
     <!-- /app_section -->
     <div style="display: none;" id="map"></div>
 
