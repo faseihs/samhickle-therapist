1b1432f95ffa515bce80a37363d229972f869a2e
Faseih Saad <fsaad.bese15seecs@seecs.edu.pk>
2019-06-20 13:32:52 +0500

    DOne

--- resources/views/layouts/therapist.blade.php
+++ resources/views/layouts/therapist.blade.php
@@ -229,7 +229,7 @@
                     </a>
                 </p>
             </div>--}}
-            <div class="col-lg-3 col-md-4">
+            <div class="col-lg-2 col-md-2">
 
                 <ul class="links">
                     <li><a style="color:#999;" href="/">Home</a></li>
@@ -238,7 +238,7 @@
 
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-4">
+            <div class="col-lg-2 col-md-2">
                 {{--
                                 <h5>About</h5>
                 --}}
@@ -248,7 +248,7 @@
                     <li><a href="/therapist/login">Therapist Login</a></li>
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-4">
+            <div class="col-lg-2 col-md-2">
 {{--
                 <h5>About</h5>
 --}}
@@ -258,14 +258,7 @@
                     <li><a href="/login">Patient Login</a></li>
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-4">
-                <h5>Contact with Us</h5>
-                <ul class="contacts">
-{{--
-                    <li><a href="tel://61280932400"><i class="icon_mobile"></i> + 61 23 8093 3400</a></li>
---}}
-                    <li><a href="mailto:help@therapist.co.uk"><i class="icon_mail_alt"></i> help@therapist.co.uk</a></li>
-                </ul>
+            <div class="col-lg-3 col-md-3">
                 <div class="follow_us">
                     <h5>Follow us</h5>
                     <ul>
@@ -276,6 +269,16 @@
                     </ul>
                 </div>
             </div>
+            <div class="col-lg-3 col-md-3 text-left">
+                <h5>Contact with Us</h5>
+                <ul class="contacts">
+                    {{--
+                                        <li><a href="tel://61280932400"><i class="icon_mobile"></i> + 61 23 8093 3400</a></li>
+                    --}}
+                    <li><a href="mailto:help@therapist.co.uk"><i class="icon_mail_alt"></i> help@therapist.co.uk</a></li>
+                </ul>
+            </div>
+
         </div>
         <!--/row-->
         <hr>
