075bb10ba642956099d437b889aacdd0c48426c7
Faseih Saad <fsaad.bese15seecs@seecs.edu.pk>
2019-06-26 13:29:29 +0500

    CHange

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        if(env('APP_ENV')=='production')
+        if(env('APP_ENV')!='local')
             \URL::forceScheme('https');
     }
 }
